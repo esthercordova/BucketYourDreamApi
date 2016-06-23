3a3846a2c9d55aed1643aba67ee10cb3e30b947c
esthercordova <estherpiacordova@gmail.com>
2016-06-23 10:27:05 -0400

    Add status column (done or not) to items table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623141010) do
+ActiveRecord::Schema.define(version: 20160623142505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160623141010) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.boolean  "status"
   end
 
   create_table "users", force: :cascade do |t|
